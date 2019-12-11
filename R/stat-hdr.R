e9af8c0f1d28ea0cdd6901efea4353a4de796639
Mitchell <mail@mitchelloharawild.com>
2019-12-12 10:32:59 +1100

    Added stat-hdr structure

---
+++ R/stat-hdr.R
@@ -0,0 +1,39 @@
+#' @export
+stat_hdr <- function(mapping = NULL, data = NULL,
+                         geom = "hdr_boxplot", position = "dodge2",
+                         ...,
+                         coef = 1.5,
+                         na.rm = FALSE,
+                         show.legend = NA,
+                         inherit.aes = TRUE) {
+  layer(
+    data = data,
+    mapping = mapping,
+    stat = StatHdr,
+    geom = geom,
+    position = position,
+    show.legend = show.legend,
+    inherit.aes = inherit.aes,
+    params = list(
+      na.rm = na.rm,
+      coef = coef,
+      ...
+    )
+  )
+}
+
+
+#' @format NULL
+#' @usage NULL
+#' @export
+StatHdr <- ggproto("Stat???", Stat,
+                       required_aes = c("y"),
+                       # non_missing_aes = "weight",
+                       setup_data = function(data, params) {
+                         # How are missing values handled?
+                       },
+
+                       compute_group = function(data, scales, width = NULL, na.rm = FALSE) {
+                         # ???
+                       }
+)

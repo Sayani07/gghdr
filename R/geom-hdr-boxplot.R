abd9c9e86c8c5e2e2a9ba566efcd095499b8c6ec
Mitchell <mail@mitchelloharawild.com>
2019-12-12 15:56:37 +1100

    Add xmin and xmax from width

--- R/geom-hdr-boxplot.R
+++ R/geom-hdr-boxplot.R
@@ -48,14 +48,18 @@ geom_hdr_boxplot <- function(mapping = NULL, data = NULL,
 #' @export
 GeomHdrBoxplot <- ggproto("GeomHdrBoxplot", Geom,
 
-                       # If we're doing custom width, we need this:
                        # need to declare `width` here in case this geom is used with a stat that
                        # doesn't have a `width` parameter (e.g., `stat_identity`).
                        extra_params = c("na.rm", "width"),
 
                        setup_data = function(data, params) {
-                         #
+                         data$width <- data$width %||%
+                           params$width %||% (resolution(data$x, FALSE) * 0.9)
 
+                         data$xmin <- data$x - data$width / 2
+                         data$xmax <- data$x + data$width / 2
+
+                         data$width <- NULL
                          data$outliers <- NULL
                          data
                        },

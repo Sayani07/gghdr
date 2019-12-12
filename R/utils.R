4fd1f1b19462a050a4dce610fe3c0f6ea1888405
Mitchell <mail@mitchelloharawild.com>
2019-12-12 23:25:03 +1100

    Improved colour matching

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,7 @@
 
 darken_fill <- function(col, prob){
   col <- farver::convert_colour(t(grDevices::col2rgb(col)), "RGB", "HSL")
-  col[,3] <- prob*100
+  col[,3] <- seq(0, 20, length.out = length(unique(prob)))[match(prob, sort(unique(prob)))] + 70
   col <- farver::convert_colour(col, "HSL", "RGB")
   col2hex(col)
 }

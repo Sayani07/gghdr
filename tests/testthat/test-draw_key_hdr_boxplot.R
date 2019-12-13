c3d3ac08ecf5a50191f03c76da4f4975504c146a
Ryo-N7 <ryonakagawara@gmail.com>
2019-12-13 12:25:18 +1100

    delete test-geom-hrd-boxplot.r as duplicate test file

--- tests/testthat/test-draw_key_hdr_boxplot.R
+++ tests/testthat/test-draw_key_hdr_boxplot.R
@@ -1,3 +1,18 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+# Setting of legend key glyphs has to be tested visually
+context("Legend key glyphs")
+
+
+test_that("alternative key glyphs work", {
+  # specify key glyph by name
+  expect_doppelganger("hdr boxplot key glyphs",
+                      ggplot(df, aes(x, y)) +
+                        geom_hdr_boxplot(key_glyph = "timeseries")
+  )
+
+  # specify key glyph by function
+  expect_doppelganger("rectangle and dotplot key glyphs",
+                      ggplot(df, aes(x, y)) +
+                        geom_line(aes(color = "line"), key_glyph = draw_key_rect) +
+                        geom_point(aes(fill = z), pch = 21, size = 3, stroke = 2, key_glyph = draw_key_dotplot)
+  )
 })
--- tests/testthat/test-hdr-boxplot.R
+++ tests/testthat/test-hdr-boxplot.R
@@ -5,7 +5,8 @@ context("hdr-box-plot")
 test_that("hdr box plot draws correctly", {
 
   hdr_boxplot <- ggplot(faithful, aes(y = eruptions)) +
-    geom_hdr_boxplot(position = "identity")
+    geom_hdr_boxplot() +
+    theme_bw()
 
   vdiffr::expect_doppelganger("hdr boxplot", hdr_boxplot)
 })

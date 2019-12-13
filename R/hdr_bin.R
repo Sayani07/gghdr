1978cea01b2a1e138c62c944e8d907a633d90efa
Mitchell <mail@mitchelloharawild.com>
2019-12-13 15:29:35 +1100

    Added 2d hdr_bin

--- R/hdr_bin.R
+++ R/hdr_bin.R
@@ -8,21 +8,31 @@
 #' @return probability coverage for each element of the numeric vectors.
 #'
 #' @export
-hdr_bin <- function(x, y, prob = c(0.5, 0.9, 0.99), ...){
-  hdr_x <- hdrcde::hdr(x, prob = prob*100)
-  hdr_box <- hdr_boxes((hdr_x))
-  prob <- rep(sort(prob, decreasing = TRUE), length.out = nrow(hdr_box))
-  df <- cbind(hdr_box, prob)[order(prob),]
+hdr_bin <- function(x, y = NULL, prob = c(0.5, 0.9, 0.99), ...){
+  if (is.null(y)) {
+    hdr_x <- hdrcde::hdr(x, prob = prob * 100, ...)
+    hdr_box <- hdr_boxes((hdr_x))
+    prob <- rep(sort(prob, decreasing = TRUE), length.out = nrow(hdr_box))
+    df <- cbind(hdr_box, prob)[order(prob),]
 
-  within_box <- function(x, df){
-    for (box in split(df, row(df))){
-      if(x >= box[1] && x <= box[2])
-        return(scales::percent(box[3]))
+    within_box <- function(x, df){
+      for (box in split(df, row(df))){
+        if(x >= box[1] && x <= box[2])
+          return(scales::percent(box[3]))
+      }
+      return(paste0(">", scales::percent(max(df[,3]))))
     }
-    return(paste0(">", scales::percent(max(df[,3]))))
-  }
 
-  vapply(x, within_box, character(1L), df=df)
+    vapply(x, within_box, character(1L), df=df)
+  }
+  else{
+    prob <- sort(prob)
+    hdr_xy <- hdrcde::hdr.2d(x, y, prob = prob * 100, ...)
+    region <- rep(-1, length(x))
+    for (i in seq_along(prob))
+      region[hdr_xy$fxy > hdr_xy$falpha[i]] <- rev(prob)[i]
+    factor(region, levels = c(prob, -1), labels = c(scales::percent(prob, 1), paste0(">", scales::percent(max(prob), 1))))
+  }
 }
 
 

2d7c08347d939fd4fee1c03c39ac581d2002233a
Sayani Gupta <Sayani.Gupta@monash.edu>
2022-01-20 15:16:36 +1100

    conditional densities added from cde branch

--- R/geom-hdr-boxplot.R
+++ R/geom-hdr-boxplot.R
@@ -1,9 +1,7 @@
 #' @title A box plot for the highest density region
-#' @details Calculates and plots the boxplot of highest density regions in one
-#' dimension.
+#' @details Calculates and plots the boxplot of highest density regions in one dimension.
 #' @param varwidth width, Default: FALSE
-#' @param prob Probability coverage required for HDRs,
-#' Default: c(0.5, 0.95, 0.99)
+#' @param prob Probability coverage required for HDRs, Default: c(0.5, 0.95, 0.99)
 #' @rdname geom_hdr_boxplot
 #' @importFrom ggplot2 layer aes
 #' @importFrom ggplot2 layer aes
@@ -14,6 +12,9 @@
 #'
 #' ggplot(faithful, aes(y = eruptions)) +
 #'   geom_hdr_boxplot()
+#' ggplot(faithful, aes(y = eruptions, x= waiting)) +
+#' geom_hdr_boxplot(fill="steelblue") +
+#' theme_minimal()
 #' @export
 
 geom_hdr_boxplot <- function(mapping = NULL, data = NULL,
@@ -29,18 +30,16 @@ geom_hdr_boxplot <- function(mapping = NULL, data = NULL,
   ## if values passed to 'prob' are integers instead of
   ## decimals, convert them to decimals
   ## ex. 5 >>> 0.05, 95 >>> 0.95
-  if (any(prob > 100 | prob < 0)) {
+  if(any(prob > 100 | prob < 0)) {
     stop(
-      "Probability values should not exceed 100 or be below 0. Please make
-      sure the values are between 0 and 1.",
+      "Probability values should not exceed 100 or be below 0. Please make sure the values are between 0 and 1.",
       call. = FALSE
     )
   }
 
-  if (any(prob > 1)) {
+  if(any(prob > 1)) {
     warning(
-      "Probability values should be on a scale between 0 to 1. If not,
-      values will be converted to decimal values.",
+      "Probability values should be on a scale between 0 to 1. If not, values will be converted to decimal values.",
       call. = FALSE
     )
     prob <- prob / 100
@@ -78,74 +77,76 @@ geom_hdr_boxplot <- function(mapping = NULL, data = NULL,
 
 GeomHdrBoxplot <- ggproto("GeomHdrBoxplot", Geom,
 
-  # need to declare `width` here in case this geom is used with a stat that
-  # doesn't have a `width` parameter (e.g., `stat_identity`).
-  extra_params = c("na.rm", "width"),
+                          # need to declare `width` here in case this geom is used with a stat that
+                          # doesn't have a `width` parameter (e.g., `stat_identity`).
+                          extra_params = c("na.rm", "width"),
 
-  setup_data = function(data, params) {
-    data$width <- data$width %||%
-      params$width %||% (resolution(data$x, FALSE) * 0.9)
+                          setup_data = function(data, params) {
+                            data$width <- data$width %||%
+                              params$width %||% (resolution(data$x, FALSE) * 0.9)
 
-    data$xmin <- data$x - data$width / 2
-    data$xmax <- data$x + data$width / 2
+                            data$xmin <- data$x - data$width / 2
+                            data$xmax <- data$x + data$width / 2
 
-    data$width <- NULL
-    data$outliers <- NULL
-    data
-  },
+                            #                         data$width <- NULL
+                            #                         data$outliers <- NULL
+                            data
+                          },
 
-  draw_group = function(data, panel_params, coord, varwidth = FALSE,
-                        prob = c(0.5, 0.95, 0.99)) {
-    fill_shade <- darken_fill(
-      rep_len(
-        data$fill,
-        length(data$prob[[1]])
-      ),
-      data$prob[[1]]
-    )
-    common <- list(
-      size = data$size,
-      linetype = data$linetype,
-      group = data$group,
-      alpha = NA
-    )
+                          draw_group = function(data, panel_params, coord, varwidth = FALSE,
+                                                prob = c(0.5, 0.95, 0.99)) {
 
-    box <- tibble::as_tibble(c(
-      list(
-        xmin = data$xmin + 0.1 * (data$xmax - data$xmin),
-        xmax = data$xmax - 0.1 * (data$xmax - data$xmin),
-        ymin = data$box[[1]][, "lower"],
-        ymax = data$box[[1]][, "upper"],
-        fill = scales::alpha(fill_shade, data$alpha),
-        colour = NA
-      ),
-      common
-    ))
+                            fill_shade <- darken_fill(rep_len(data$fill, length(data$prob[[1]])), data$prob[[1]])
+                            common <- list(
+                              size = data$size,
+                              linetype = data$linetype,
+                              group = data$group,
+                              alpha = NA
+                            )
 
-    mode <- tibble::as_tibble(c(
-      list(
-        x = data$xmin,
-        xend = data$xmax,
-        y = data$mode[[1]],
-        yend = data$mode[[1]],
-        colour = data$colour
-      ),
-      common
-    ), n = length(data$mode[[1]]))
+                            num_boxes <- sapply(data$box, nrow)
+                            num_probs <- sapply(data$prob, length)
+                            #ignore dodged widths if doing cde
+                            if (nrow(data) > 1) {
+                              data$xmin <- data$x_cde - data$width / 2
+                              data$xmax <- data$x_cde + data$width / 2
+                            }
 
-    ggplot2:::ggname("geom_hdr_boxplot", grid::grobTree(
-      ggplot2::GeomRect$draw_panel(box, panel_params, coord),
-      ggplot2::GeomSegment$draw_panel(mode, panel_params, coord)
-    ))
-  },
+                            box <- tibble::as_tibble(c(
+                              list(
+                                #Expand out packed HDR box specifications
+                                xmin = rep(data$xmin + 0.1* (data$xmax-data$xmin), times = num_boxes),
+                                xmax = rep(data$xmax - 0.1* (data$xmax-data$xmin), times = num_boxes),
+                                ymin = unlist(lapply(data$box, function(b) b[,"lower"])),
+                                ymax = unlist(lapply(data$box, function(b) b[,"upper"])),
+                                fill = rep(scales::alpha(fill_shade, unique(data$alpha)), length.out = sum(num_probs)),
+                                colour = NA
+                              ),
+                              lapply(common, rep, length.out = sum(num_boxes))
+                            ))
 
-  draw_key = draw_key_hdr_boxplot,
+                            mode <- tibble::as_tibble(c(
+                              list(
+                                x = data$xmin,
+                                xend = data$xmax,
+                                y = unlist(data$mode),
+                                yend = unlist(data$mode),
+                                colour = data$colour
+                              ),
+                              common
+                            ), n = length(data$mode[[1]]))
 
-  default_aes = aes(
-    weight = 1, colour = "grey20", fill = "black", size = 0.5,
-    alpha = NA, shape = 19, linetype = "solid", prob = NA
-  ),
+                            ggplot2:::ggname("geom_hdr_boxplot", grid::grobTree(
+                              ggplot2::GeomRect$draw_panel(box, panel_params, coord),
+                              ggplot2::GeomSegment$draw_panel(mode, panel_params, coord)
+                            ))
+                          },
 
-  required_aes = c("ymax", "ymin", "box"),
-  optional_aes = "prob"
+                          draw_key = draw_key_hdr_boxplot,
+
+                          default_aes = aes(weight = 1, colour = "grey20", fill = "black", size = 0.5,
+                                            alpha = NA, shape = 19, linetype = "solid", prob = NA),
+
+                          required_aes = c("ymax", "ymin", "box"),
+                          optional_aes = "prob"
 )
